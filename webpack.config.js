ca1d59c1f755b1af4408b88ae6b833ad3018a6b2
stalink456 <stas.litvin2010@yandex.ru>
2023-05-25 16:45:32 +0300

    change env for vercel

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,9 @@ module.exports = {
     new ESLintPlugin({
       extensions: ['js', 'jsx', 'ts', 'tsx'],
     }),
-    new Dotenv(),
+    new Dotenv({
+      systemvars: true,
+    }),
   ],
   devServer: {
     historyApiFallback: true,
